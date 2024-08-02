27cea9435b728edd439713bb3d69f2f70008458f
WintressUK <corywinter13@gmail.com>
2024-08-02 16:27:58 +0100

    Prelim support for lat/lon entry + relevant UI. No integration with plotting yet.

--- experiment.py
+++ experiment.py
@@ -39,6 +39,15 @@ apply_mask = tk.BooleanVar()
 negis_lats = pd.read_csv("latbook.csv", header = None)
 negis_lons = pd.read_csv("lonbook.csv", header = None)
 
+latitude_found = False
+longitude_found = False
+step = 0
+
+selected_lat_variable = None
+selected_lon_variable = None
+lat_data = None
+lon_data = None
+
 def print_pink(text): #define function for printing output text in pink
     pink_colour_code = '\033[95m' 
     reset_colour_code = '\033[0m'
@@ -72,8 +81,246 @@ def load_file():
             print_pink(f'File loaded successfully.')
             update_loaded_file()
         except Exception as e:
-            print_pink(f'Error loading file.')
-         
+            print_pink(f'An error occured: {e}')
+            
+#initialises the popup box for the flowchart of latitude/longitude selection
+def initalise_latitude_popup():
+    global step
+    step = 0 #reset the step whenever the latitude window opens
+    latitude_button.config(state=tk.DISABLED)
+    auto_latitude_detect() #initalise the process
+    
+def clear_window(window):
+    for widget in window.winfo_children():
+        widget.destroy()
+        
+def on_top_window_close():
+    latitude_button.config(state=tk.NORMAL)
+    top.destroy()
+    
+def next():
+    global step, latitude_found, longitude_found
+    step += 1
+    clear_window(top)
+    
+    if latitude_found and longitude_found:
+        print_pink("Latitude and Longitude found.")
+        top.destroy()
+        latitude_button.config(state=tk.NORMAL)
+        selected_latitude_label.config(text="Lat/Lon Uploaded.", fg="#32CD32")
+        print_pink(lat_data)
+        print_pink(lon_data)
+    elif step == 1:
+        manual_variable_select()
+    elif step == 2: 
+        upload_lat_lon_file()
+    elif step == 3:
+        manual_scale_entry()
+    else:
+        top.destroy()
+        latitude_button.config(state=tk.NORMAL)
+          
+#popup box executing auto detection of lat/lon in file
+def auto_latitude_detect():
+    global top, latitude_found, longitude_found
+    top = tk.Toplevel(root)
+    #top.geometry("750x250")
+    top.title("Scale Selection")
+    top.protocol("WM_DELETE_WINDOW", on_top_window_close)
+    
+    if ncx is not None:
+        global lat_data, lon_data
+        
+        auto_latitude_detect_label = tk.Label(top, text= "Auto Scale Detection")
+        auto_latitude_detect_label.pack(pady=5)
+    
+        for var in ncx.variables:
+            if var in ['lat', 'lats']:
+                lat_data = ncx[var]
+                latitude_variable_name = var
+                latitude_found = True
+                break
+            
+        if lat_data == None:
+            no_latitude_label = tk.Label(top, text = "No Latitudes Autodetected.")
+            no_latitude_label.pack(pady =5)
+        elif lat_data is not None:
+            latitude_label = tk.Label(top, text=f"Latitudes Detected from Variable {latitude_variable_name}")
+            latitude_label.pack(pady=5)
+            
+        for var in ncx.variables:
+            if var in ['lon', 'lons']:
+                lon_data = ncx[var]
+                longitude_variable_name = var
+                longitude_found = True
+                break
+                
+        if lon_data == None:
+            no_longitude_label = tk.Label(top, text = "No Longitudes Autodetected.")
+            no_longitude_label.pack(pady =5)
+        elif lon_data is not None:
+            longitude_label = tk.Label(top, text=f"Longitudes Detected from Variable {longitude_variable_name}")
+            longitude_label.pack(pady=5)
+            
+        auto_next_button = tk.Button(top, text="Next", command=next)
+        auto_next_button.pack(pady=5)
+    
+    else:
+        nothing_selected_label = tk.Label(top, text= "Please load a file.")
+        nothing_selected_label.pack(pady=5)
+        
+def manual_variable_select(): #STEP 1
+    global manual_variable_select_combobox, manual_variable_select_combobox_lon, latitude_found, longitude_found, lat_data, lon_data
+    
+    manual_variable_select_top_label = tk.Label(top, text = "Manual Select Lat and Lon Variable")
+    manual_variable_select_top_label.pack(pady=5)
+    
+    lat_frame = tk.Frame(top)
+    lon_frame = tk.Frame(top)
+    
+    lat_frame.pack(pady=5)
+    lon_frame.pack(pady=5)
+    
+    manual_variable_select_combobox_label = tk.Label(lat_frame, text = "Select Latitude Variable")
+    manual_variable_select_combobox_label.pack(side=tk.LEFT, padx=5, pady=10)
+    
+    manual_variable_select_combobox = Combobox(lat_frame, state="readonly", values = list(ncx.variables))
+    manual_variable_select_combobox.pack(pady=10)
+    manual_variable_select_combobox.bind("<<ComboboxSelected>>", on_lat_variable_select)
+    
+    manual_variable_select_combobox_label_lon = tk.Label(lon_frame, text = "Select Longitude Variable")
+    manual_variable_select_combobox_label_lon.pack(side=tk.LEFT, padx=5, pady=10)
+    
+    manual_variable_select_combobox_lon = Combobox(lon_frame, state="readonly", values = list(ncx.variables))
+    manual_variable_select_combobox_lon.pack(pady=10)
+    manual_variable_select_combobox_lon.bind("<<ComboboxSelected>>", on_lon_variable_select)
+    
+    if selected_lon_variable:
+        longitude_found = True
+        lon_data = ncx[selected_lon_variable]
+    if selected_lat_variable:
+        latitude_found = True
+        lat_data = ncx[selected_lat_variable]
+        
+    manual_variable_select_next = tk.Button(top, text="Next", command=next)
+    manual_variable_select_next.pack(pady=5)
+    
+def upload_lat_lon_file(): #STEP 2
+    global latitude_found, longitude_found, loaded_lat_label, loaded_lon_label
+    upload_lat_lon_file_top_label = tk.Label(top, text = "Upload Lat/Lon File")
+    upload_lat_lon_file_top_label.pack(pady=5)
+    
+    lat_frame = tk.Frame(top)
+    lon_frame = tk.Frame(top)
+    
+    lat_frame.pack(pady=5)
+    
+    lat_label = tk.Label(lat_frame, text = "Select Latitude File")
+    lat_label.pack(side=tk.LEFT, padx=5, pady=10)
+    
+    lat_file_button = tk.Button(lat_frame, text="Upload File", command=load_lat_file)
+    lat_file_button.pack(pady=5)
+    
+    loaded_lat_label = tk.Label(top, text = "Latitude File: None")
+    loaded_lat_label.pack(pady=5)
+    
+    lon_frame.pack(pady=5)
+    
+    lon_label = tk.Label(lon_frame, text = "Select Longitude File")
+    lon_label.pack(side=tk.LEFT, padx=5, pady=10)
+    
+    lon_file_button = tk.Button(lon_frame, text="Upload File", command=load_lon_file)
+    lon_file_button.pack(pady=5)
+    
+    loaded_lon_label = tk.Label(top, text = "Longitude File: None")
+    loaded_lon_label.pack(pady=5)
+        
+    #next button
+    next_button = tk.Button(top, text="Next", command=next)
+    next_button.pack(pady=5)
+      
+def load_lat_file():
+    global lat_data, latitude_found
+    lat_file_path = filedialog.askopenfilename(
+        filetypes=[("CSV files", "*.csv")]
+    )
+    lat_data = pd.read_csv(lat_file_path)
+    latitude_found = True
+    
+    if lat_data is not None:
+        loaded_lat_label.config(text=f"Latitude File: {lat_file_path}", fg="#32CD32")
+    
+def load_lon_file():
+    global lon_data, longitude_found
+    lon_file_path = filedialog.askopenfilename(
+        filetypes=[("CSV files", "*.csv")]
+    )
+    lon_data = pd.read_csv(lon_file_path)
+    longitude_found = True
+    
+    if lon_data is not None:
+        loaded_lon_label.config(text=f"Latitude File: {lon_file_path}", fg="#32CD32")
+        
+def manual_scale_entry(): #STEP 3
+    global min_lat_entry, max_lat_entry, min_lon_entry, max_lon_entry
+    lat_frame = tk.Frame(top)
+    lat_frame.pack(pady=5)
+    
+    min_lat_label = tk.Label(lat_frame, text="Min Lat")
+    min_lat_label.pack(side=tk.LEFT, padx=5)
+        
+    min_lat_entry = tk.Entry(lat_frame)
+    min_lat_entry.pack(side=tk.LEFT, padx=5)
+    
+    max_lat_label = tk.Label(lat_frame, text="Max Lat")
+    max_lat_label.pack(side=tk.LEFT, padx=5)
+        
+    max_lat_entry = tk.Entry(lat_frame)
+    max_lat_entry.pack(side=tk.LEFT, padx=5)
+    
+    lon_frame = tk.Frame(top)
+    lon_frame.pack(pady=5)
+    
+    min_lon_label = tk.Label(lon_frame, text="Min Lon")
+    min_lon_label.pack(side=tk.LEFT, padx=5)
+        
+    min_lon_entry = tk.Entry(lon_frame)
+    min_lon_entry.pack(side=tk.LEFT, padx=5)
+    
+    max_lon_label = tk.Label(lon_frame, text="Max Lon")
+    max_lon_label.pack(side=tk.LEFT, padx=5)
+        
+    max_lon_entry = tk.Entry(lon_frame)
+    max_lon_entry.pack(side=tk.LEFT, padx=5)
+    
+    #next button (presented as quit button)
+    buttons_frame = tk.Frame(top)
+    buttons_frame.pack(pady=5)
+    
+    confirm_button = tk.Button(top, text="Confirm", command=confirm_manual_scale)
+    confirm_button.pack(side=tk.LEFT, padx=5)
+    
+    next_button = tk.Button(top, text="Quit", command=next)
+    next_button.pack(side=tk.LEFT, padx=5)
+    
+def confirm_manual_scale():
+    global min_lat, max_lat, min_lon, max_lon
+    global latitude_found, longitude_found
+    global lat_data, lon_data
+    
+    min_lat = int(min_lat_entry.get())
+    max_lat = int(max_lat_entry.get())
+    min_lon = int(min_lon_entry.get())
+    max_lon = int(max_lon_entry.get())
+    
+    latitude_found = True
+    longitude_found = True
+    
+    lat_data = [min_lat, max_lat]
+    lon_data = [min_lon, max_lon]
+    
+    next() #call next function to check latitude_found and longitude_found again
+    
 #add a dropdown menu to select variables, based upon the file loaded
 def update_variable_dropdown():
     if ncx:
@@ -100,6 +347,14 @@ def on_variable_select(event):
 def on_mask_variable_select(event):
     global selected_mask_variable
     selected_mask_variable = mask_variable_dropdown.get()
+    
+def on_lat_variable_select(event):
+    global selected_lat_variable
+    selected_lat_variable = manual_variable_select_combobox.get()
+    
+def on_lon_variable_select(event):
+    global selected_lon_variable
+    selected_lon_variable = manual_variable_select_combobox_lon.get()
 
 #enables and disabled the timestep options based on whether single timestep or range is selected        
 def toggle_timestep_mode():
@@ -112,6 +367,17 @@ def toggle_timestep_mode():
         single_timestep_entry.config(state=tk.DISABLED)
         start_timestep_entry.config(state=tk.NORMAL)
         end_timestep_entry.config(state=tk.NORMAL)
+        
+#shows and hides masking options based on whether apply mask box is checked
+def toggle_masking_mode():
+    if apply_mask.get():
+        mask_variable_dropdown.config(state=tk.NORMAL)
+        masking_range_start_entry.config(state=tk.NORMAL)
+        masking_range_end_entry.config(state=tk.NORMAL)
+    else:
+        mask_variable_dropdown.config(state=tk.DISABLED)
+        masking_range_start_entry.config(state=tk.DISABLED)
+        masking_range_end_entry.config(state=tk.DISABLED)
 
 #changes the label that shows the current timestep range 
 def update_timestep_range():
@@ -121,7 +387,7 @@ def update_timestep_range():
 #changes the label that shows the current loaded file       
 def update_loaded_file():
     if ncx is not None:
-        loaded_file_label.config(text=f"Loaded File: {file_path}")
+        loaded_file_label.config(text=f"Loaded File: {file_path}", fg="#32CD32")
 
 #assigns selected colourmap to an object
 def update_colourmap(event):
@@ -173,17 +439,6 @@ def confirm_selection():
     else:
         messagebox.showwarning("Warning", "Please make sure all selections are made.")
         
-#shows and hides masking options based on whether apply mask box is checked
-def toggle_masking_mode():
-    if apply_mask.get():
-        mask_variable_dropdown.config(state=tk.NORMAL)
-        masking_range_start_entry.config(state=tk.NORMAL)
-        masking_range_end_entry.config(state=tk.NORMAL)
-    else:
-        mask_variable_dropdown.config(state=tk.DISABLED)
-        masking_range_start_entry.config(state=tk.DISABLED)
-        masking_range_end_entry.config(state=tk.DISABLED)
- 
 #procedure for collating multiple plots into an animation using pillow library       
 def create_gif():
     print_pink("Creating GIF...")
@@ -394,16 +649,23 @@ def on_close():
 def main():
     global root, variable_dropdown, mask_variable_dropdown, colourmap_dropdown, timestep_range_label_text, timestep_mode, single_timestep_entry, start_timestep_entry, end_timestep_entry
     global negis_lats, negis_lons, ncx, apply_mask, mask_frame, masking_range_start_entry, masking_range_end_entry, loaded_file_label
+    global latitude_button, selected_latitude_label
 
     try:
         negis_lats = pd.read_csv("latbook.csv", header=None)
         negis_lons = pd.read_csv("lonbook.csv", header=None)
 
         load_button = tk.Button(root, text="Load File", command=load_file)
-        load_button.pack(pady=20)
+        load_button.pack(pady=5)
         
         loaded_file_label = tk.Label(root, text="Loaded File: None")
         loaded_file_label.pack(pady=5)
+        
+        latitude_button = tk.Button(root, text="Load Lat/Lon", command=initalise_latitude_popup)
+        latitude_button.pack(pady=5)
+        
+        selected_latitude_label = tk.Label(root, text="No lat/lon loaded.")
+        selected_latitude_label.pack(pady=5)
 
         variable_dropdown_label = tk.Label(root, text="Select Variable:")
         variable_dropdown_label.pack(pady=5)
@@ -501,5 +763,4 @@ def main():
     except Exception as e:
         print(f"An error occurred: {e}")
         
-
 main()
